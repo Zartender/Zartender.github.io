12d431ef32f0dd0d9f5f1bb9dc506b02144c1730
Ian Greenblott <imgreenblott@gmail.com>
2021-12-12 18:07:29 -0500

    Testing.

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -5,21 +5,35 @@ class Header extends Component {
     render() {
         return (
             <header className="App-header">
-  
-        <p>
-         Edit <code>src/App.js</code> and save to reload.
-        </p>        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-            );
-          }
-        }
-        
-        export default Header;
+
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                    <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+                    <ul id="nav" className="nav">
+                        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        <li><a className="smoothscroll" href="#about">About</a></li>
+                        <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                        <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+
+
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>        <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        );
+    }
+}
+
+export default Header;
