cede6fa493566fa3b66597c1709cb84cb80fcb86
Ian Greenblott <imgreenblott@gmail.com>
2021-12-12 23:54:06 -0500

    little Adjustments.

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
+
+
 class Header extends Component {
     render() {
         return (
@@ -12,7 +14,7 @@ class Header extends Component {
                     <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
                     <ul id="nav" className="nav">
-                        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        <li><a className="smoothscroll" href="#home">Home</a></li>
                         <li><a className="smoothscroll" href="#about">About</a></li>
                         <li><a className="smoothscroll" href="#resume">Resume</a></li>
                         <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
@@ -20,10 +22,8 @@ class Header extends Component {
                     </ul>
                     <h1>Ian Greenblott</h1>
                 </nav>
-
-
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                  <img src={logo} className="App-logo-mini" alt="logo" />
                 </p>        <a
                     className="App-link"
                     href="https://reactjs.org"
@@ -32,7 +32,7 @@ class Header extends Component {
                 >
                     Learn React
                 </a>
-                <img src={logo} className="App-logo" alt="logo" />
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
             </header>
             </div>
         );
